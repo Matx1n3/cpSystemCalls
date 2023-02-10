e500c3caae3ee483656749bf9e7fb3cc37f6e0e4
matxinn03 <matxinj@gmail.com>
2023-02-10 12:57:06 +0100

    Actually using system calls

--- main.c
+++ main.c
@@ -1,41 +1,49 @@
-#include <stdio.h>
+#include <sys/fcntl.h>
 #include <unistd.h>
-#include <fcntl.h>
 
-void main(int argc, char *argv[])
-{
-	char buf;
-	int fd_one, fd_two;
 
-    //  if argc != 3 reject
-    if (argc != 3){
-        printf("syntax: copySC fileFrom fileTo\n");
-        return;
+void print(char text[], int size){
+    write(STDOUT_FILENO, text, size);
+}
+
+
+int main(int argc, char *argv[]) {
+
+    //  If argc != 3 reject
+    if (argc != 3) {
+        print("Please enter the file you want to copy and the file where you want to copy it\n", 78);
+        return 0;
     }
 
-	fd_one = open(argv[1], O_RDONLY);
+    //  Open the file copyFrom
+    int copyFrominodeNum = open(argv[1], O_RDONLY);
 
-    //Check that the first file can be opened
-	if (fd_one == -1)
-	{
-		printf("Error opening first_file\n");
-		close(fd_one);
-		return;
-	}
+    //  Open the file copyTo
+    int copyToinodeNum = open(argv[2], O_CREAT|O_WRONLY, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
 
-    //  Open the second file
-	fd_two = open(argv[2],O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    //  Make sure that the copyFrom file exists
+    if (copyFrominodeNum == -1) {
+        print("The specified file doesn't exist\n", 33);
+        return 0;
+    }
 
-    //  Copy
-	while(read(fd_one, &buf, 1))
-	{
-		write(fd_two, &buf, 1);
-	}
+    //  Make sure there hasn't been any problem with copyTo
+    if (copyToinodeNum == -1){
+        print("Something went wrong\n", 21);
+        return 0;
+    }
 
-    //  Operation successful
-	printf("Successful copy\n");
+    //  Copy the data
+    char ch;
+    while (read(copyFrominodeNum, &ch, 1)) {
+        write(copyToinodeNum, &ch, 1);
+    }
 
-    //  Close files
-	close(fd_one);
-	close(fd_two);
+    // Close files
+    close(copyFrominodeNum);
+    close(copyToinodeNum);
+
+    //  Successful message
+    print("Successfull\n", 12);
 }
+
